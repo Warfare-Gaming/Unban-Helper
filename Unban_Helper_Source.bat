9105c098d0e80262df01f7021ce7ab0c060b8c23
Abhay SV <monuabhay@yahoo.in>
2021-05-30 02:08:17 +0530

    Added Registry Path check

--- Unban_Helper_Source.bat
+++ Unban_Helper_Source.bat
@@ -1,7 +1,18 @@
 @echo off
+
+if exist gta_sa.exe goto yesfile
+if not exist gta_sa.exe goto regpath
+goto end
+
+:regpath
+for /f "tokens=3" %%a in ('reg query "HKEY_CURRENT_USER\Software\SAMP"  /V gta_sa_exe  ^|findstr /ri "REG_SZ"') do echo %%a > gta_path.txt
+set /p GTA_PATH=<gta_path.txt
+del gta_path.txt
+copy /y wgCrypt.exe %GTA_PATH:~0,-11%
+cd /D %GTA_PATH:~0,-11%
 if exist gta_sa.exe goto yesfile
 if not exist gta_sa.exe goto nofile
-goto end
+
 :yesfile
 color 1f
 del wg.txt
@@ -10,23 +21,38 @@ echo ---------------------------------------------------------------------------
 echo WELCOME TO WARFARE GAMING UPBAN APPEAL HELPER TOOL
 echo -------------------------------------------------------------------------------------
 echo Press Any key to continue
+if exist wgCrypt.exe goto genfile
+if not exist gta_sa.exe goto nowgcrypt
 :genfile
-tree /f /a > wg.txt
-zip unban_app.dat wg.txt
-del wg.txt
+for /f "tokens=3" %%a in ('reg query "HKEY_CURRENT_USER\Software\SAMP"  /V gta_sa_exe  ^|findstr /ri "REG_SZ"') do echo %%a > gta_path.txt
+tree /f /a > tree_wg.txt
+dir /T:W > time_wg.txt
+wgCrypt.exe unban_app.dat tree_wg.txt time_wg.txt gta_path.txt
+pause
+del tree_wg.txt
+del time_wg.txt
+del gta_path.txt
 cls 
 color 2f
 echo -------------------------------------------------------------------------------------
 echo PLAESE UPLOAD THE FILE NAMED unban_app.dat IN YOUR BAN APPEAL
 echo VISIT : WARFARE-GAMING.COM/FORUM
 echo -------------------------------------------------------------------------------------
-pause
+goto end
 :nofile
 color 4f
 echo -------------------------------------------------------------------------------------
 echo INCORRECT FOLDER SAMP NOT FOUND
 echo PLEASE PASTE THIS PROGRAMME IN YOUR GTA FOLDER AND THEN RUN IT
 echo -------------------------------------------------------------------------------------
+pause
+goto end
+:nowgcrypt
+color 4f
+echo -------------------------------------------------------------------------------------
+echo wgCrypt.exe NOT FOUND
+echo PLEASE PASTE ALL THE FILES IN YOUR GTA FOLDER AND THEN RUN IT
+echo --------------------------------------------------------------------------------------
 pause
 goto end
 :end
